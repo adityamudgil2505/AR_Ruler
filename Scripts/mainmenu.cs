0cbc3d4d9b2a0d712e29af9adca0bd648ded51a8
adityamudgil2505 <adityamudgil2505@gmail.com>
2018-07-21 00:07:14 +0530

    Added new files
    Script for developing AR_Ruler in C#

---
+++ Scripts/mainmenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour {
+
+	public void Startt()
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void Quit()
+	{
+		Application.Quit();
+	}
+	public void mainm()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+}
